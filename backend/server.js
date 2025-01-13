0c2a44ab2b2974803ad0782c8d89715aa6485f2c
taynguyen3110 <taynguyen3110@gmail.com>
2025-01-13 15:49:51 +1100

    test 2025

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,9 +20,8 @@ app.use('/', createKeyRouter);
 const importKeyRouter = require('./routes/importKey');
 app.use('/', importKeyRouter);
 
-// 'getEngineVer.js' 라우터 추가
 const getEngineVerRouter = require('./routes/getEngineVer');
-app.use('/', getEngineVerRouter); // '/' 경로에 라우터를 추가합니다.
+app.use('/', getEngineVerRouter);
 
 const deployAWSRouter = require('./routes/deployAWS');
 app.use('/', deployAWSRouter);
@@ -47,7 +48,7 @@ const getMetricRouter = require('./routes/getMetricData');
 app.use('/', getMetricRouter);
 
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const port = 5000;
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/userpool.js
+++ src/userpool.js
@@ -1,6 +1,6 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 const poolData = {
-  UserPoolId: "ap-southeast-2_bCcmBKJEl",
-  ClientId: "5k9p6lrabtc2uicsblc3t7vvll",
+  UserPoolId: "ap-southeast-2_t7EfS4ej7",
+  ClientId: "26uhbp04i5p7l6imfig976u0a8",
 };
 export default new CognitoUserPool(poolData);
